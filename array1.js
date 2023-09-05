42e281c43c8fb32c4ce3f240d0d1aa2b1616eb72
diaz-11 <angiediazvilca12@gmail.com>
2023-09-05 15:51:11 -0300

    array1

---
+++ array1.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5];
+numbers[0] = 10;
+numbers[2] = 30;
+console.log(numbers); // [10, 2, 30, 4, 5]
+
+// ¿Qué se imprimirá en la consola al ejecutar el siguiente código?
+//se remplaza los valores iniciales con los nuevos valores, en este caso el valor 0 se remplaza
+//por el valor 10, y el valor 2 se remplaza con el 30.
